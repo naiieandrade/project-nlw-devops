d6fd879f84f8c646b71874fdc5ce056ec5a2f84d
Naiara Andrade <andradenaiara@hotmail.com>
2024-04-07 21:50:51 -0300

    fix: get health route in swagger

--- nlw.service.passin/src/http/server.ts
+++ nlw.service.passin/src/http/server.ts
@@ -11,7 +11,7 @@ import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
-import { errorHandler } from "./error-handler";
+import { errorHandler } from "./errors";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
