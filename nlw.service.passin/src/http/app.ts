39ef6f3b43ee140d46abc399c5502da606df75e0
Naiara Andrade <andradenaiara@hotmail.com>
2024-04-07 22:18:37 -0300

    update: update app file

--- nlw.service.passin/src/http/app.ts
+++ nlw.service.passin/src/http/app.ts
@@ -1,11 +1,57 @@
-import { env } from '@/env'
-import { app } from './app'
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
-app
-  .listen({
-    host: '0.0.0.0',
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log(`HTTP server running! (http://localhost:3333/docs)`)
-  })
+import { errorHandler } from './errors'
+import { createEvent } from './routes/create-event'
+import { getEvent } from './routes/get-event'
+import { getEventAttendees } from './routes/get-event-attendees'
+import { registerForEvent } from './routes/register-for-event'
+import { getAttendeeBadge } from './routes/get-attendee-badge'
+import { checkIn } from './routes/check-in'
+import { health } from './routes/health'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description:
+        'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors, {
+  origin: '*',
+  credentials: true,
+})
+
+app.setErrorHandler(errorHandler)
+
+app.register(createEvent)
+app.register(getEvent)
+app.register(getEventAttendees)
+app.register(registerForEvent)
+app.register(getAttendeeBadge)
+app.register(checkIn)
+app.register(health)
